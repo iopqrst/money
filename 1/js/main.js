6cb22a0a7fcd6571aaaa830761f352fad431f4d5
iopqrst <opqrst@163.com>
2015-06-10 21:14:51 +0800

    #init

---
+++ 1/js/main.js
@@ -0,0 +1,41 @@
+(function($) {
+	$('#S_show_pop').click(function() {
+		if ($('#us_panel_menu').hasClass('show')) {
+			$(".dsx_price").show();
+			$('#us_panel_menu').removeClass('show');
+			$('.arrow_top').removeClass('open');
+			$(window).unbind('touchmove');
+		} else {
+			$(".dsx_price").hide();
+			$('#us_panel_menu').addClass('show');
+			$('.arrow_top').addClass('open');
+			$(window).bind('touchmove', function(e) {
+				e.preventDefault();
+				e.stopImmediatePropagation();
+			});
+		}
+	});
+
+	$('.us_panel_msk').click(function() {
+		$('#S_show_pop').trigger('click');
+	});
+	
+	$("#S_submit").click(function(){
+		$('#S_show_pop').trigger('click');
+	});
+
+	// tab
+	$(".add").click(function() {
+		// $(this).prev() 就是当前元素的前一个元素，即 text_box
+		$(this).prev().val(parseInt($(this).prev().val()) + 1);
+	});
+	
+	$(".min").click(function() {
+		// $(this).next() 就是当前元素的下一个元素，即 text_box
+		if ($(this).next().val() > 1) {
+			$(this).next().val(parseInt($(this).next().val()) - 1);
+		} else {
+			$(this).next().val(1);
+		}
+	});
+})(Zepto);
